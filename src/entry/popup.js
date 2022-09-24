78f0f65337dc25fd5de0ceb98bb9d43a5448d942
Damian Chng <damianchng@damians-mbp.lan>
2022-09-24 16:09:19 +0800

    added vuecourse template component

--- src/entry/popup.js
+++ src/entry/popup.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import popupView from '../view/popup.vue'
+import Course from '../models/Course.js'
 
-createApp(popupView, {msg: "Test"}).mount('#app')
+let t = [new Course("english", 10, "english"), new Course("test", 10, "test"), new Course("test", 10, "test"), new Course("test", 10, "test"), new Course("test", 10, "test"), new Course("test", 10, "test")];
+console.log(t);
+createApp(popupView, {courses: t}).mount('#app')
